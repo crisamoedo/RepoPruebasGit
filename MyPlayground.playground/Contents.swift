2cdd7b645f7dce6fa328a95f68af9d67a438859b
Cristina Amoedo <gizanakris@gmail.com>
2023-09-28 13:35:01 +0200

    Probando control de flujo en Swift

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -3,8 +3,20 @@ import Cocoa
 var greeting = "Vamos a hacer operaciones"
 var num1 = 5
 var num2 = 6
-
+var myInt = 6
+var myString = "Este es mi String de prueba"
 
 
 print(greeting)
 print(num1 + num2)
+
+
+print("Probando control de flujo en Swift")
+
+if myInt == 10 && myString == "Hola" {
+    print("El valor es 10")
+}else if myInt == 11 || myString == "Hola" {
+    print("El valor es 11")
+}else{
+    print("El valor no es 10 ni 11")
+}
